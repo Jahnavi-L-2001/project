e718fa70b5ce5fbfec54f3cb564f15b1d6ee4d2c
Jahnavi <janavi.1919@gmail.com>
2026-08-08 17:37:17 +0530

    Add TRIM() to name columns in staging models, remove redundant self-aliases

--- models/staging/stg_family.sql
+++ models/staging/stg_family.sql
@@ -1,11 +1,11 @@
 SELECT
-  parent_name      AS parent_name,
-  gender           AS parent_gender,
-  dob              AS parent_dob,
-  city             AS city,
-  state            AS state,
-  house_number     AS house_number,
-  office_phone     AS office_phone,
-  personal_phone   AS personal_phone,
-  kid_name         AS kid_name
+  TRIM(parent_name)      AS parent_name,
+  gender                 AS parent_gender,
+  dob                    AS parent_dob,
+  city,
+  state,
+  house_number,
+  office_phone,
+  personal_phone,
+  kid_name
 FROM {{ source('raw', 'family') }}
